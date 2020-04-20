dfb3449bfefe084d2b21c891ec224208f9f86c0d
NoteFox <kitsu-geissler@t-online.de>
2020-04-20 20:34:03 +0200

    first HomeFragmentTest

---
+++ app/src/main/java/de/htwBerlin/ois/Fragments/AboutActivity.java
@@ -0,0 +1,49 @@
+package de.htwBerlin.ois.Fragments;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.bottomnavigation.LabelVisibilityMode;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import de.htwBerlin.ois.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private static final String TAG = "AboutActivity";
+    BottomNavigationView bottom_navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/de/htwBerlin/ois/Fragments/MapDownloadFragment.java
@@ -0,0 +1,117 @@
+package de.htwBerlin.ois.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import de.htwBerlin.ois.FTP.AsyncResponse;
+import de.htwBerlin.ois.FTP.FtpEndpointSingleton;
+import de.htwBerlin.ois.FTP.FtpTaskFileListing;
+import de.htwBerlin.ois.FileStructure.OhdmFile;
+import de.htwBerlin.ois.FileStructure.OhdmFileAdapter;
+import de.htwBerlin.ois.MainActivityPackage.Database;
+import de.htwBerlin.ois.R;
+
+/**
+ * This Activity represents a small map file download center
+ */
+public class MapDownloadFragment extends Fragment {
+
+    private static final String TAG = "MapDownloadActivity";
+
+    private static final String FTP_SERVER_IP = "";
+    private static final Integer FTP_PORT = 21;
+    private static final String FTP_USER = "";
+    private static final String FTP_PASSWORD = "";
+
+    private ListView listView;
+
+    private ArrayList<OhdmFile> ohdmFiles;
+    private FtpEndpointSingleton ftpEndpointSingleton;
+    /* private FtpTaskFileListing ftpTaskFileListing = new FtpTaskFileListing(new AsyncResponse() {
+        @Override
+        public void getOhdmFiles(ArrayList<OhdmFile> files) {
+            ohdmFiles.addAll(files);
+            Log.i(TAG, "received " + files.size() + " files.");
+            OhdmFileAdapter adapter = new OhdmFileAdapter(Database.mainContext.getApplicationContext(), R.layout.adapter_view_layout, ohdmFiles);
+            listView.setAdapter(adapter);
+        }
+    }, this);
+    */
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_map_download, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ohdmFiles = new ArrayList<>();
+
+        listView = view.findViewById(R.id.downladRecycler);
+
+        initializeFTPSingleton();
+        listFTPFiles();
+
+    }
+
+    /**
+     * Initializes FTP Endpoint singleton
+     */
+    private void initializeFTPSingleton() {
+        ftpEndpointSingleton = FtpEndpointSingleton.getInstance();
+        ftpEndpointSingleton.setFtpPassword(FTP_PASSWORD);
+        ftpEndpointSingleton.setFtpUser(FTP_USER);
+        ftpEndpointSingleton.setServerIp(FTP_SERVER_IP);
+        ftpEndpointSingleton.setServerPort(FTP_PORT);
+    }
+
+    /**
+     * executes list files async task
+     */
+    private void listFTPFiles() {
+        //ftpTaskFileListing.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+}
