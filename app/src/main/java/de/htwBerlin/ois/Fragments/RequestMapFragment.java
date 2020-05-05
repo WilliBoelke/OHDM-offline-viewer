a996cc4108c7353704b8253bf6136946418d84b7
willi <williboelke@outlook.com>
2020-05-05 20:21:25 +0200

    added EditText views fo get 8 coordinates from the user

--- app/src/main/java/de/htwBerlin/ois/Fragments/RequestMapFragment.java
+++ app/src/main/java/de/htwBerlin/ois/Fragments/RequestMapFragment.java
@@ -1,11 +1,13 @@
 package de.htwBerlin.ois.Fragments;
 
-import android.support.v4.app.Fragment;
-
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import de.htwBerlin.ois.R;
 
@@ -15,6 +17,36 @@ public class RequestMapFragment extends Fragment
      * The View
      */
     private View view;
+    /**
+     * Date picker to let the user pick the date of the map
+     */
+    private DatePicker datePicker;
+
+    /**
+     * Coordinate pair 1 / upper left corner
+     */
+    private EditText coordx1;
+    private EditText coordy1;
+
+    /**
+     * Coordinate pair 2 / upper right corner
+     */
+    private EditText coordx2;
+    private EditText coordy2;
+
+    /**
+     * Coordinate pair 3 / bottom right corner
+     */
+    private EditText coordx3;
+    private EditText coordy3;
+
+    /**
+     * Coordinate pair 4 / bottom left corner
+     */
+    private EditText coordx4;
+    private EditText coordy4;
+
+    private EditText name;
 
 
     @Override
@@ -24,5 +56,45 @@ public class RequestMapFragment extends Fragment
         return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        this.setupDatePicker();
 
+
+        name = view.findViewById(R.id.name_et);
+    }
+
+
+
+    /**
+     * Setup for the date picker
+     */
+    private void setupDatePicker()
+    {
+        datePicker = view.findViewById(R.id.simpleDatePicker);
+        //no calender view
+        datePicker.setCalendarViewShown(false);
+
+    }
+
+    /**
+     * Building a formatted string (jjjj-mm-dd), as its needed by the server
+     *
+     * @return Date as string
+     */
+    private String getDatePickerValuesAsString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        //Building a string in format JJJJJ-MM-TT
+        stringBuilder.append(datePicker.getYear());
+        stringBuilder.append("-");
+        stringBuilder.append(datePicker.getMonth());
+        stringBuilder.append("-");
+        stringBuilder.append(datePicker.getDayOfMonth());
+
+        return stringBuilder.toString();
+    }
 }
