bccf374839cd7269b4e48dcfac98fd4775803883
willi <williboelke@outlook.com>
2020-05-08 20:31:26 +0200

    New nad improved HomeFragment, Fixed that the Botom navigation didnt show up in the navigation fragment

--- app/src/main/java/de/htwBerlin/ois/Fragments/NavigationFragment.java
+++ app/src/main/java/de/htwBerlin/ois/Fragments/NavigationFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import org.mapsforge.core.model.LatLong;
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
@@ -15,6 +16,7 @@ import org.mapsforge.map.datastore.MapDataStore;
 import org.mapsforge.map.layer.cache.TileCache;
 import org.mapsforge.map.layer.renderer.TileRendererLayer;
 import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.reader.header.MapFileException;
 import org.mapsforge.map.rendertheme.InternalRenderTheme;
 
 import java.io.File;
@@ -65,8 +67,7 @@ public class NavigationFragment extends Fragment
     private void setUpMap()
     {
         Log.i(TAG, "setting up map");
-        mapView = new MapView(getActivity().getApplicationContext());
-        getActivity().setContentView(mapView);
+        mapView = view.findViewById(R.id.map);
 
         mapView.setClickable(true);
         mapView.getMapScaleBar().setVisible(true);
@@ -75,14 +76,23 @@ public class NavigationFragment extends Fragment
         TileCache tileCache = AndroidUtil.createTileCache(getActivity().getApplicationContext(), "mapcache", mapView.getModel().displayModel.getTileSize(), 1f, mapView.getModel().frameBufferModel.getOverdrawFactor());
 
         File ohdmFile = MapFileSingleton.getInstance().getFile();
-        MapDataStore mapDataStore = new MapFile(ohdmFile);
+        MapDataStore mapDataStore = null;
+        try
+        {
+            mapDataStore = new MapFile(ohdmFile);
 
-        TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore, mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
-        tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.DEFAULT);
+            TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore, mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
+            tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.DEFAULT);
 
-        mapView.getLayerManager().getLayers().add(tileRendererLayer);
-        mapView.setZoomLevel((byte) 12);
-        mapView.setCenter(new LatLong(52.517037, 13.38886));
+            mapView.getLayerManager().getLayers().add(tileRendererLayer);
+            mapView.setZoomLevel((byte) 12);
+            mapView.setCenter(new LatLong(52.517037, 13.38886));
+        }
+        catch (MapFileException e)
+        {
+            Toast.makeText(getActivity().getApplicationContext(), "Your map file is invalid", Toast.LENGTH_LONG).show();
+        }
+
     }
 
 
