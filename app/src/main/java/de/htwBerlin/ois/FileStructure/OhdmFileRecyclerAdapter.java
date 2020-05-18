76c62ea01c4930a3a792f7d31601375fca1d8a78
willi <williboelke@outlook.com>
2020-05-18 21:04:25 +0200

    Added: search in MapDownloadFragment

--- app/src/main/java/de/htwBerlin/ois/FileStructure/OhdmFileRecyclerAdapter.java
+++ app/src/main/java/de/htwBerlin/ois/FileStructure/OhdmFileRecyclerAdapter.java
@@ -3,27 +3,42 @@ package de.htwBerlin.ois.FileStructure;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import de.htwBerlin.ois.R;
 
-public class OhdmFileRecyclerAdapter extends RecyclerView.Adapter<OhdmFileRecyclerAdapter.OhdmFileViewHolder>
+import static android.support.constraint.Constraints.TAG;
+
+public class OhdmFileRecyclerAdapter extends RecyclerView.Adapter<OhdmFileRecyclerAdapter.OhdmFileViewHolder> implements Filterable
 {
+    /**
+     * This list will be altered when the user searches for maps
+     */
     private ArrayList<OhdmFile> mapArrayList;
-    private Context context;
+    /**
+     * This list will always contain all maps
+     * Its here as a backup for the mapArrayList
+     */
+    private ArrayList<OhdmFile> mapArrayListBackup;
     private int ressource;
     private OnItemClickListener onItemClickListener;
 
-    public OhdmFileRecyclerAdapter(Context context, ArrayList<OhdmFile> ohdmFiles, int ressource)
+
+    public OhdmFileRecyclerAdapter(Context context, ArrayList<OhdmFile> ohdmFiles, ArrayList<OhdmFile >mapArrayListBackup,  int ressource)
     {
         this.ressource = ressource;
         this.mapArrayList = ohdmFiles;
-        this.context = context;
+        Log.e(TAG, "Size... " + mapArrayList.size());
+        this.mapArrayListBackup = mapArrayListBackup;// needs to be initialized like that so i dosnt point to the same list as mapArrayList
+        Log.e(TAG, "Size... " + this.mapArrayListBackup.size());
     }
 
     @NonNull
@@ -61,11 +76,53 @@ public class OhdmFileRecyclerAdapter extends RecyclerView.Adapter<OhdmFileRecycl
         this.onItemClickListener = listener;
     }
 
-
     public interface OnItemClickListener
     {
         void onItemClick(int position);
     }
+
+    @Override
+    public Filter getFilter()
+    {
+        return nameFilter;
+    }
+
+    private Filter nameFilter = new Filter()
+    {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint)
+        {
+            ArrayList<OhdmFile> filteredList = new ArrayList<>();
+            Log.e(TAG, "Size--" + mapArrayListBackup.size());
+            if (constraint == null || constraint.length() == 0)
+            {
+                filteredList.addAll(mapArrayListBackup);
+            }
+            else
+            {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                Log.e(TAG, "Size _____" + mapArrayListBackup.size());
+                for (OhdmFile map : mapArrayListBackup)
+                {
+                    if (map.getFilename().toLowerCase().trim().contains(filterPattern))
+                    {
+                        filteredList.add(map);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results)
+        {
+            mapArrayList.clear();
+            mapArrayList.addAll((ArrayList) results.values);
+            notifyDataSetChanged();
+        }
+    };
 
 
     protected static class OhdmFileViewHolder extends RecyclerView.ViewHolder
@@ -101,7 +158,7 @@ public class OhdmFileRecyclerAdapter extends RecyclerView.Adapter<OhdmFileRecycl
             });
 
         }
-    }
 
+    }
 
 }
--- app/src/main/java/de/htwBerlin/ois/Fragments/MapDownloadFragment.java
+++ app/src/main/java/de/htwBerlin/ois/Fragments/MapDownloadFragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,6 +25,8 @@ import de.htwBerlin.ois.R;
 import de.htwBerlin.ois.ServerCommunication.AsyncResponse;
 import de.htwBerlin.ois.ServerCommunication.FtpTaskFileDownloading;
 import de.htwBerlin.ois.ServerCommunication.FtpTaskFileListing;
+
+import static java.util.Collections.addAll;
 
 /**
  * This Activity represents a small map file download center
@@ -55,8 +58,10 @@ public class MapDownloadFragment extends Fragment
      * ArrayList of OHDMFiles, to be displayed in the RecyclerView
      */
     private ArrayList<OhdmFile> ohdmFiles;
+    private ArrayList<OhdmFile>ohdmFilesBackup;
     private ItemTouchHelper itemTouchHelper;
     private FloatingActionButton requestNewMapFab;
+    private SearchView searchView;
     /**
      * The view
      */
@@ -75,8 +80,44 @@ public class MapDownloadFragment extends Fragment
     public void onActivityCreated(@Nullable Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
-        this.setupRecyclerView();
+        this.setupSearchView();
         this.listFTPFiles();
+        this.setupRecyclerView();
+        this.setupFAB();
+
+    }
+
+    private void setupFAB()
+    {
+        requestNewMapFab = view.findViewById(R.id.request_new_map_fab);
+        requestNewMapFab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RequestMapFragment()).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    private void setupSearchView()
+    {
+        searchView = view.findViewById(R.id.map_search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                recyclerAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 
     /**
@@ -92,6 +133,7 @@ public class MapDownloadFragment extends Fragment
         recyclerView = view.findViewById(R.id.available_maps_recycler);
         recyclerView.setVisibility(View.INVISIBLE);
         ohdmFiles = new ArrayList<>();
+        ohdmFilesBackup = new ArrayList<>();
         recyclerLayoutManager = new LinearLayoutManager(this.getContext());
         itemTouchHelper = new ItemTouchHelper(new RecyclerViewItemSwipeGestures(recyclerAdapter, new LeftSwipeCallback()
         {
@@ -103,10 +145,19 @@ public class MapDownloadFragment extends Fragment
                 recyclerAdapter.notifyDataSetChanged();
             }
         }));
+
+
+        view.findViewById(R.id.connecting_tv).setVisibility(View.INVISIBLE);
+        view.findViewById(R.id.connecting_pb).setVisibility(View.INVISIBLE);
         itemTouchHelper.attachToRecyclerView(recyclerView);
         recyclerView.setLayoutManager(recyclerLayoutManager);
-        recyclerAdapter = new OhdmFileRecyclerAdapter(getActivity().getApplicationContext(), ohdmFiles, R.layout.download_recycler_item);
+        recyclerAdapter = new OhdmFileRecyclerAdapter(getActivity().getApplicationContext(), ohdmFiles, ohdmFilesBackup, R.layout.download_recycler_item);
         recyclerView.setAdapter(recyclerAdapter);
+        recyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void listFTPFiles()
+    {
         FtpTaskFileListing ftpTaskFileListing = new FtpTaskFileListing(new AsyncResponse()
         {
             @Override
@@ -114,11 +165,10 @@ public class MapDownloadFragment extends Fragment
             {
                 if (files.size() > 0)
                 {
-                    ohdmFiles.addAll(files);
                     Log.i(TAG, "received " + files.size() + " files.");
+                    ohdmFiles.addAll(files);
+                    ohdmFilesBackup.addAll(files);
                     recyclerView.setVisibility(View.VISIBLE);
-                    view.findViewById(R.id.connecting_tv).setVisibility(View.INVISIBLE);
-                    view.findViewById(R.id.connecting_pb).setVisibility(View.INVISIBLE);
                     recyclerAdapter.notifyDataSetChanged();
                 }
                 else
@@ -130,35 +180,13 @@ public class MapDownloadFragment extends Fragment
         }, getActivity());
         ftpTaskFileListing.execute();
 
-
-        //FloatingActionButton
-
-        requestNewMapFab = view.findViewById(R.id.request_new_map_fab);
-        requestNewMapFab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RequestMapFragment()).addToBackStack(null).commit();
-            }
-        });
     }
-
-    /**
-     * executes list files async task
-     */
-    private void listFTPFiles()
-    {
-        //ftpTaskFileListing.execute();
-    }
-
 
     @Override
     public void onStart()
     {
         super.onStart();
     }
-
 
     @Override
     public void onResume()
